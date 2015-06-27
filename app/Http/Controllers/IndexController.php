3d584aef5d8dfe6bec0c20152d5dbcf561945438
Zach Q <zach.queal@gmail.com>
2015-06-27 00:22:46 -0400

    'init'

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class IndexController # extends Controller
+{
+    public function index() {
+      return view('index');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Lumen Boiler Plate
+
+A simple PHP boilerplate for the [Lumen](http://lumen.laravel.com/) framework.
